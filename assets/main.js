2e6a85759269b765e77d28d4b9cc20d7845be534
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2020-09-26 16:11:40 +0800

    Update website design

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,16 @@ document.querySelectorAll(".hero-gallery").forEach(gallery => {
         img.src = select.value;
     });
 });
+var menuButton = document.querySelector('.menu-button')
+var sidebar = document.querySelector('.sidebar')
+var contentOverlay = document.querySelector('.content__overlay')
+
+menuButton.addEventListener('click', function () {
+  sidebar.classList.toggle('sidebar--is-visible')
+  contentOverlay.classList.toggle('content__overlay--is-active')
+})
+
+contentOverlay.addEventListener('click', function () {
+  sidebar.classList.toggle('sidebar--is-visible')
+  contentOverlay.classList.toggle('content__overlay--is-active')
+})
