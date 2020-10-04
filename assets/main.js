95cb44a87e2b2930437b80bc7b0eb112d5324db4
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2020-10-04 20:18:00 +0800

    Click to toggle skill name translation

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,14 @@
 document.querySelectorAll(".hero-gallery").forEach(gallery => {
-    const sprites = JSON.parse(gallery.dataset.arts);
-    const select = gallery.querySelector(".spriteSelector");
-    const img = gallery.querySelector("img");
-    for (const s of sprites) {
-        const x = s.split("/");
-        select.add(new Option(x[x.length - 1], s));
-    }
-    select.addEventListener("change", e => {
-        img.src = select.value;
-    });
+  const sprites = JSON.parse(gallery.dataset.arts);
+  const select = gallery.querySelector(".spriteSelector");
+  const img = gallery.querySelector("img");
+  for (const s of sprites) {
+    const x = s.split("/");
+    select.add(new Option(x[x.length - 1], s));
+  }
+  select.addEventListener("change", e => {
+    img.src = select.value;
+  });
 });
 var menuButton = document.querySelector('.menu-button')
 var sidebar = document.querySelector('.sidebar')
@@ -43,3 +43,10 @@ document.querySelectorAll(".skill-trigger").forEach(trigger => {
   }
   trigger.textContent = "triggered when " + f.join(" and ");
 });
+function toggleTranslate(e) {
+  const ele = e.target;
+  const tmp = ele.textContent;
+  ele.textContent = ele.dataset.translate;
+  ele.dataset.translate = tmp;
+}
+document.querySelectorAll(".translate").forEach(t => t.addEventListener('click', toggleTranslate));
