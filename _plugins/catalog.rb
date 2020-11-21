35b903aa5cc73e04b623523fddba41f3146f37fc
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2020-11-21 13:48:30 +0800

    Implement voice actor and illustrator page

---
+++ _plugins/catalog.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Jekyll
+
+  module CatalogFilter
+    def processVoiceActor(name)
+      if name == '？？？'
+        name = '天野ユウ,岩永悠平,戸板優衣,樹元オリエ'
+      end
+      name.split(",")
+    end
+
+    def processCharaGroup(cards, key, maps)
+      group = {}
+      cards.each do |card|
+        name = card[key]
+        names = processVoiceActor(name)
+
+        names.each do |n|
+          array = group[n]
+          if !array
+            array = []
+            group[n] = array
+          end
+
+          array.push(card)
+        end
+      end
+
+      final = []
+      group.each do |name, items|
+        final.push({"name" => name, "en" => maps[name]&.fetch("en"), "items" => items})
+      end
+
+      final
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::CatalogFilter)
