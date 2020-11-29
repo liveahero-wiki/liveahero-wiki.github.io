887c8df1d5a312cbdff959aa9737e04257d65464
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2020-11-29 12:32:18 +0800

    5* Hero Stats ranking

--- assets/main.js
+++ assets/main.js
@@ -55,3 +55,36 @@ document.querySelectorAll("[data-expiry]").forEach(d => {
   const dd = new Date(d.dataset.expiry);
   if (dd < today) d.classList.add("expired");
 });
+function sortTable(table, column, btn) {
+  const oldOrder = parseInt(btn.dataset.order || '-1', 10);
+  const newOrder = 0 - oldOrder;
+  btn.dataset.order = newOrder;
+
+  const array = [], rows = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    if (i == 0) continue;
+    const s = rows[i].cells[column].textContent;
+    const value = btn.dataset.type == "string" ? s : parseInt(s, 10);
+    array.push({value: value, element: rows[i]});
+  }
+  array.sort((lhs, rhs) => {
+    return lhs.value > rhs.value ? newOrder : lhs.value < rhs.value ? oldOrder : 0;
+  });
+
+  for (const x of array) {
+    table.appendChild(x.element);
+  }
+}
+document.querySelectorAll(".sort-table").forEach(T => {
+  const heads = T.querySelectorAll("th");
+  let i = 0;
+  heads.forEach(h => {
+    const k = i++;
+    h.tabIndex = 0;
+    h.addEventListener("keydown", (e) => { if (e.key == 'Enter') {
+      e.preventDefault();
+      sortTable(T, k, h);
+    }});
+    h.addEventListener("click", () => sortTable(T, k, h));
+  })
+});
