c7fce795204dc09bdcffa6c2910eb37da8893925
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2024-05-03 17:22:58 +0800

    Sales report code [ci skip]

---
+++ tools/sales_report.py
@@ -0,0 +1,22 @@
+import re
+
+CHAR_SUB = re.compile("{(\d+)}", re.DOTALL)
+
+def processOneReport(key, value):
+  value = re.sub(CHAR_SUB, "<code>character\\1</code>", value)
+
+  return f"""<details><summary>{key}</summary>
+<p>{value}</p></details>
+"""
+
+def main():
+  with open("sales_report.txt", "w", encoding="utf-8") as w:
+    with open("_data/processed/Japanese.properties", "r") as f:
+      for l in f:
+        key, value = l.split("=", 1)
+        if key.startswith("SALES_REPORT_") or key.startswith("SALES_EVENT_"):
+          w.write(processOneReport(key, value))
+          w.write("\n")
+
+if __name__ == '__main__':
+  main()
