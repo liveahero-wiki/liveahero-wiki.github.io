62cc9626b8a19d815c9a55de3d0ad88f1dd168eb
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2025-10-08 21:42:42 +0800

    fix

--- assets/chart.js
+++ assets/chart.js
@@ -1,6 +1,6 @@
 const chartElements = document.querySelectorAll(".apache-chart");
 for (const chartElement of chartElements) {
   const E = echarts.init(chartElement);
-  const options = JSON.parse(E.dataset.options);
+  const options = JSON.parse(chartElement.dataset.options);
   E.setOption(options);
 }
