867e8812df98ffab566aa1f9aacd62ac557e5e2a
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2025-10-08 22:32:16 +0800

    resize

--- assets/chart.js
+++ assets/chart.js
@@ -1,6 +1,13 @@
 const chartElements = document.querySelectorAll(".apache-chart");
+const EE = [];
 for (const chartElement of chartElements) {
   const E = echarts.init(chartElement);
   const options = JSON.parse(chartElement.dataset.options);
   E.setOption(options);
+  EE.push(E);
 }
+window.addEventListener('resize', () => {
+  for (const E of EE) {
+    E.resize();
+  }
+})
