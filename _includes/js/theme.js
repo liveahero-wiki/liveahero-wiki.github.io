9d808c9098effb8f0f216adf9d187a09993ef17c
Rong Jie <71772241+rongjie-alter@users.noreply.github.com>
2025-12-07 14:33:27 +0800

    fix syntax

--- _includes/js/theme.js
+++ _includes/js/theme.js
@@ -1,38 +1,36 @@
 const themeKey = 'theme-preference';
 
-const onClick = () => {
-  theme.value = theme.value === 'light' ? 'dark' : 'light';
-  setPreference();
-}
-
 const getColorPreference = () => {
-  if (localStorage.getItem(themeKey)) return localStorage.getItem(themeKey)
-  else return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-}
+  if (localStorage.getItem(themeKey)) return localStorage.getItem(themeKey);
+  else return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
 
 const setPreference = () => {
-  localStorage.setItem(themeKey, theme.value)
-  reflectPreference()
-}
+  localStorage.setItem(themeKey, theme.value);
+  reflectPreference();
+};
 
 const reflectPreference = () => {
-  document.firstElementChild.setAttribute('data-theme', theme.value)
-  document.querySelector('#theme-toggle')?.setAttribute('aria-label', theme.value)
-}
+  document.firstElementChild.setAttribute('data-theme', theme.value);
+  document.querySelector('#theme-toggle')?.setAttribute('aria-label', theme.value);
+};
 
 const theme = {
   value: getColorPreference(),
-}
+};
 
-reflectPreference()
+reflectPreference();
 
-window.onload = () => {
-  reflectPreference()
+document.addEventListener('DOMContentLoaded', () => {
+  reflectPreference();
 
-  document.querySelector('#theme-toggle').addEventListener('click', onClick)
-}
+  document.querySelector('#theme-toggle').addEventListener('click', () => {
+    theme.value = theme.value === 'light' ? 'dark' : 'light';
+    setPreference();
+  });
+};
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({matches:isDark}) => {
-  theme.value = isDark ? 'dark' : 'light'
-  setPreference()
-})
+  theme.value = isDark ? 'dark' : 'light';
+  setPreference();
+});
